9c8cb6e5d902df0f621d86999c890230fcfe976c
Manoj Kumar <2023.manoj.sudalai@ves.ac.in>
2024-04-07 01:07:37 +0530

    changed code to print

--- sum.py
+++ sum.py
@@ -1,15 +1,1 @@
-def main():
-    # Input first number
-    num1 = float(input("Enter the first number: "))
-
-    # Input second number
-    num2 = float(input("Enter the second number: "))
-
-    # Calculate the sum
-    total = num1 + num2
-
-    # Display the result
-    print("The sum of", num1, "and", num2, "is:", total)
-
-if __name__ == "__main__":
-    main()
+print("hello in jenkins");
