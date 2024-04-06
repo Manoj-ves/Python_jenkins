c288967cf1c6c10a06bdb35c33dede3920d2a709
Manoj Kumar <2023.manoj.sudalai@ves.ac.in>
2024-04-07 01:24:03 +0530

    sum

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-from flask import Flask
-
-# Create a Flask application
-app = Flask(__name__)
-
-# Define a route for the root URL
-@app.route('/')
-def index():
-    return 'Hello, World! This is my Flask app running on localhost:8080.'
-
-if __name__ == '__main__':
-    # Run the Flask application on localhost, port 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    print("Flask app is running on port 5000")
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num1 : "))
+print("Sum is ",num1+num2)
